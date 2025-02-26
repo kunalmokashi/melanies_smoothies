a448e5488523ab6c95d0f1b591f608a1ad13b9f7
kunalmokashi <kmokashi.7@gmail.com>
2025-02-26 15:04:42 -0500

    Update streamlit_app.py
    Code refactoring SmoothieFroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ if ingredients_list:
     ingredients_string = ' '
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+ name_on_order +"""')"""
@@ -33,5 +35,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
